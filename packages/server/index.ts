93d15d20aba299d8b65a9d98021905c409568359
chenxi-debugger <chenxi.debugger@gmail.com>
2025-09-07 11:41:53 -0700

    Setup Prettier

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,25 +1,20 @@
 import express from 'express';
-import type {Request, Response } from 'express';
-import dotenv from 'dotenv'
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
-
-
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello Chenxi');
-})
+   res.send('hello Chenxi');
+});
 
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({message: 'hello Chenxi'});
-})
+   res.json({ message: 'hello Chenxi' });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-
-})
-
-
+   console.log(`Server is running on http://localhost:${port}`);
+});
